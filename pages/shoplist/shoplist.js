bc908810ef01eb15d58db6eb023320410728758a
fangxu <864109504@qq.com>
2017-06-08 09:57:06 +0800

    s

--- pages/shoplist/shoplist.js
+++ pages/shoplist/shoplist.js
@@ -8,7 +8,38 @@ Page({
     data:{},
 
     onLoad: function () {
-       
+        wx.getLocation({
+            type: 'gcj02',
+            success: function(res) {
+                console.log(res);
+                var latitude = res.latitude
+                var longitude = res.longitude
+                var speed = res.speed
+                var accuracy = res.accuracy
+
+            }
+        });
+
+
+        Common.request({
+                url: "/r/Mall_Product/getTicketList/",
+                data: {
+                    lid: 212,
+                    scenCode:"wxApp#oBvKZ9",
+                },
+                loading: function () {
+                    Common.showLoading()
+                },
+                complete: function () {
+                    Common.hideLoading();
+                },
+                success: function (res) {
+                    //console.log(res);
+                    _this.setData({
+                        ticketList : res.data.list,
+                    })
+                }
+        });
     },
     onPullDownRefresh: function () {
       setTimeout(function(){
